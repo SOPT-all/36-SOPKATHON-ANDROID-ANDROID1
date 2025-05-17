a599f9c0437fbed350123c3b824c8216f0912871
kimjw2003 <kimjw111003@gmail.com>
2025-05-18 06:13:16 +0900

    aaa

--- app/src/main/java/com/sopt/at/sopkathon/team1/presentation/productdetail/ProductDetailScreen.kt
+++ app/src/main/java/com/sopt/at/sopkathon/team1/presentation/productdetail/ProductDetailScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -89,8 +90,8 @@ fun ProductDetailScreen(
                 contentDescription = null,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .aspectRatio(1.2f)
-                    .height(300.dp),
+                    .height(300.dp)
+                    .clip(RoundedCornerShape(bottomEnd = 32.dp, bottomStart = 32.dp)),
                 contentScale = ContentScale.Crop
             )
 
