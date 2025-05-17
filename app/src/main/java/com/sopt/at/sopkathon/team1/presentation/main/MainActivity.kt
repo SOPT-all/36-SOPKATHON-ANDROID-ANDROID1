01e258fb126d19759c5ba0ebc57ab1b99894fc7f
serioushyeon <zz64446949@gmail.com>
2025-05-17 21:45:35 +0900

    [chore] 테마 적용

--- app/src/main/java/com/sopt/at/sopkathon/team1/presentation/main/MainActivity.kt
+++ app/src/main/java/com/sopt/at/sopkathon/team1/presentation/main/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.sopt.at.sopkathon.team1.core.designsystem.ui.theme.Team1Theme
+import com.sopt.at.sopkathon.team1.core.designsystem.ui.theme.SopkatonTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -13,7 +13,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Team1Theme {
+            SopkatonTheme {
                 MainScreen()
             }
         }
