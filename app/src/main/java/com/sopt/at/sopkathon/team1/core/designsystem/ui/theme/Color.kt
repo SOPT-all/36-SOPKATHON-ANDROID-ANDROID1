7cd738afe0dc7c4d8996ff60cbde8202f6549e62
kimjw2003 <kimjw111003@gmail.com>
2025-05-17 21:28:28 +0900

    [ADD] Theme

--- app/src/main/java/com/sopt/at/sopkathon/team1/core/designsystem/ui/theme/Color.kt
+++ app/src/main/java/com/sopt/at/sopkathon/team1/core/designsystem/ui/theme/Color.kt
@@ -1,5 +1,7 @@
 package com.sopt.at.sopkathon.team1.core.designsystem.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +11,26 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+@Immutable
+data class SopkatonColors(
+    val Purple80: Color,
+    val PurpleGrey80: Color,
+    val Pink80: Color,
+
+    val Purple40: Color,
+    val PurpleGrey40: Color,
+    val Pink40: Color,
+)
+
+val defaultSopkatonColors = SopkatonColors(
+    Purple80 = Purple80,
+    PurpleGrey80 = PurpleGrey80,
+    Pink80 = Pink80,
+
+    Purple40 = Purple40,
+    PurpleGrey40 = PurpleGrey40,
+    Pink40 = Pink40,
+)
+
+val LocalSopkatonColorsProvider = staticCompositionLocalOf { defaultSopkatonColors }
--- app/src/main/java/com/sopt/at/sopkathon/team1/core/designsystem/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/at/sopkathon/team1/core/designsystem/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.sopt.at.sopkathon.team1.core.designsystem.ui.theme
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,50 +10,65 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 @Composable
-fun Team1Theme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+fun ProvideColorsAndTypography(
+    colors: SopkatonColors,
+    typography: SopkatonTypography,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
+    CompositionLocalProvider(
+        LocalSopkatonColorsProvider provides colors,
+        LocalTypographyProvider provides typography,
         content = content
     )
+}
+
+@Composable
+fun SopkatonTheme(
+    content: @Composable () -> Unit
+) {
+    ProvideColorsAndTypography(
+        colors = defaultSopkatonColors,
+        typography = defaultSopkatonTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = false
+                }
+            }
+        }
+        MaterialTheme(
+            content = content
+        )
+    }
+}
+
+@Composable
+fun TodomateTheme(
+    content: @Composable () -> Unit
+) {
+    ProvideColorsAndTypography(
+        colors = defaultSopkatonColors,
+        typography = defaultSopkatonTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = false
+                }
+            }
+        }
+        MaterialTheme(
+            content = content
+        )
+    }
 }
