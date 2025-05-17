de249d88d1ab2785b0c427f06f7f83056485af87
kimjw2003 <kimjw111003@gmail.com>
2025-05-17 21:17:07 +0900

    [ADD] Typo Design System

--- app/src/main/java/com/sopt/at/sopkathon/team1/core/designsystem/ui/theme/Type.kt
+++ app/src/main/java/com/sopt/at/sopkathon/team1/core/designsystem/ui/theme/Type.kt
@@ -1,34 +1,144 @@
 package com.sopt.at.sopkathon.team1.core.designsystem.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sopt.at.sopkathon.team1.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val Suit = FontFamily(
+    Font(R.font.suit_regular, FontWeight.Normal),
+    Font(R.font.suit_semibold, FontWeight.SemiBold),
+    Font(R.font.suit_extrabold, FontWeight.ExtraBold)
 )
+
+@Immutable
+data class SopkatonTypography(
+    val body_r_24: TextStyle,
+    val body_r_20: TextStyle,
+    val body_r_18: TextStyle,
+    val body_r_16: TextStyle,
+    val body_r_14: TextStyle,
+    val body_r_12: TextStyle,
+
+    val title_sb_32: TextStyle,
+    val title_sb_28: TextStyle,
+    val title_sb_24: TextStyle,
+    val title_sb_20: TextStyle,
+    val title_sb_18: TextStyle,
+    val title_sb_16: TextStyle,
+    val title_sb_14: TextStyle,
+
+    val head_eb_32: TextStyle,
+    val head_eb_28: TextStyle,
+    val head_eb_24: TextStyle,
+    val head_eb_20: TextStyle,
+    val head_eb_18: TextStyle,
+    val head_eb_16: TextStyle,
+)
+
+val defaultSopkatonTypography = SopkatonTypography(
+    body_r_24 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp
+    ),
+    body_r_20 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    body_r_18 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp
+    ),
+    body_r_16 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body_r_14 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    body_r_12 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    ),
+
+
+    title_sb_32 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 32.sp
+    ),
+    title_sb_28 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 28.sp
+    ),
+    title_sb_24 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 24.sp
+    ),
+    title_sb_20 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp
+    ),
+    title_sb_18 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp
+    ),
+    title_sb_16 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    title_sb_14 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp
+    ),
+
+    head_eb_32 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 32.sp
+    ),
+    head_eb_28 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 28.sp
+    ),
+    head_eb_24 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 24.sp
+    ),
+    head_eb_20 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 20.sp
+    ),
+    head_eb_18 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 18.sp
+    ),
+    head_eb_16 = TextStyle(
+        fontFamily = Suit,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 16.sp
+    )
+)
+
+val LocalTypographyProvider = staticCompositionLocalOf { defaultSopkatonTypography }
