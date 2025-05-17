52d19bff182454ef2d6cddad6ca92b53a52f5420
serioushyeon <zz64446949@gmail.com>
2025-05-18 02:20:10 +0900

    [add] enum 추가

--- app/src/main/java/com/sopt/at/sopkathon/team1/core/KeyStorage.kt
+++ app/src/main/java/com/sopt/at/sopkathon/team1/core/KeyStorage.kt
@@ -1,4 +1,19 @@
 package com.sopt.at.sopkathon.team1.core
 
+import com.sopt.at.sopkathon.team1.R
+
 object KeyStorage {
+    val LEVEL_NAME_LIST = listOf(
+        "노비",
+        "평민",
+        "귀족",
+        "공주"
+    )
+    val LEVEL_IMG_LIST = listOf(
+        R.drawable.img_home_level,
+        R.drawable.img_home_level,
+        R.drawable.img_home_level,
+        R.drawable.img_home_level
+    )
+
 }
